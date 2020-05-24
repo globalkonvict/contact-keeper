f78db97a057904468894eb53931984702345d3f1
Sarthak Dwivedi <sarthakwarlock@gmail.com>
2020-05-24 09:26:02 +0530

    Configuration changes

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
 
 //Init middleware
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
 //connectDB
 connectDB();
@@ -14,5 +15,14 @@ connectDB();
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contact'));
 app.use('/api/users', require('./routes/users'));
+
+//server static
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server started on Port: ${PORT}`));
